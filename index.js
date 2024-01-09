20361ce29d2175d3b48f0445780c3467a5848029
huynvq <quanghuy.vnu65@gmail.com>
2024-01-09 11:50:22 +0700

    update

---
+++ index.js
@@ -0,0 +1,36 @@
+const app = require("express")();
+
+let chrome = {};
+let puppeteer;
+
+chrome = require("chrome-aws-lambda");
+puppeteer = require("puppeteer-core");
+
+app.get("/api", async (req, res) => {
+  let options = {};
+
+  options = {
+    args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chrome.defaultViewport,
+    executablePath: await chrome.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  };
+
+  try {
+    let browser = await puppeteer.launch(options);
+
+    let page = await browser.newPage();
+    await page.goto("https://www.google.com");
+    res.send(await page.title());
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
+
+module.exports = app;
