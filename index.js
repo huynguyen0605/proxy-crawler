70d8bf16f5e0cd2caa7089b9ee6e563c107faa90
huynvq <quanghuy.vnu65@gmail.com>
2024-01-09 12:03:26 +0700

    update

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ const app = require("express")();
 let chrome = {};
 let puppeteer;
 
-chrome = require("chrome-aws-lambda");
-puppeteer = require("puppeteer-core");
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
 
 app.get("/api", async (req, res) => {
   let options = {};
 
-  options = {
-    args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-    defaultViewport: chrome.defaultViewport,
-    executablePath: await chrome.executablePath,
-    headless: true,
-    ignoreHTTPSErrors: true,
-  };
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
 
   try {
     let browser = await puppeteer.launch(options);
